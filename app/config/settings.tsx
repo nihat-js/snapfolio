16eee6df572bc54fb7b2a9449dbbb4c321ca349a
Nihat <abdullazadenihat@gmail.com>
2024-11-21 15:33:21 +0400

    Add settings configuration and integrate components for bio, skills, and featured projects

---
+++ app/config/settings.tsx
@@ -0,0 +1,43 @@
+const general = {
+  shortBio:
+    "I’m a software engineer with a passion for building scalable and high-performing web applications. I specialize in PHP and Laravel for backend development, and use JavaScript frameworks like React.js, Next.js, and Vue.js to create dynamic, responsive user interfaces.",
+  longBio: "aaa",
+  profileImage: "",
+  mail: "a",
+  gmail: "",
+};
+
+const skills = [
+  "PHP",
+  "Laravel",
+  "CodeIgniter",
+  "Solid Principles",
+  "Javascript",
+  "TypeScript",
+  "ReactJS / NextJS",
+  "NodeJS",
+  "MySQL PostgreSQL ",
+  "Git",
+  "Docker",
+  "Linux",
+  "Kubernetes",
+]
+
+const links = {
+  github: "https://github.com/nihat-js",
+  linkedin: "https://github.com/nihat-js",
+  twitter: "https://github.com/nihat-js",
+  instagram: "https://github.com/nihat-js",
+}
+
+const featuredProjects = [
+  {
+    name: "E-commerce Platform",
+    description:
+      "A full-stack e-commerce solution with React, Node.js, and MongoDB",
+    tech: "React",
+    link: "#",
+    tags: ["React", "Node.js", "MongoDB"],
+  },
+  {},
+];
