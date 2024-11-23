2413befe816378b4c58bd49c9ea063df767bc617
Nihat <abdullazadenihat@gmail.com>
2024-11-23 23:19:34 +0400

    fixed bugs

--- app/slide-puzzle/game.tsx
+++ app/slide-puzzle/game.tsx
@@ -1,29 +1,28 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./style.css";
 import index from "../games/page";
 export default function Game() {
     const [board, setBoard] = useState<number[]>();
     const [width, setWidth] = useState(4);
-    const [selectedTileIndex, setselectedTileIndex] = useState<number>(1);
+    const [selectedTileIndex, setselectedTileIndex] = useState<number>(0);
+
+    const boardRef = useRef(board);
+
 
     const handleSwitch = () => {
-        console.log({ width });
         setWidth(width == 3 ? 4 : 3);
     };
 
     function convertIndexToPosition(index: number) {
-        let y = Math.ceil(index / 4);
-        let x = index - (y - 1) * 4 + 1;
+        let y = Math.max(Math.ceil(index / width), 1);
+        let x = index - (y - 1) * width + 1;
         return [y, x];
     }
     function convertPositionToIndex(position: number[]) {
         return (position[0] - 1) * width + position[1] - 1;
     }
-    function isEmptyByPosition(position: number[]) {
-        let index = convertPositionToIndex(position);
-        return (board as [])[index] === -1;
-    }
+
     function isValidPosition(position: number[]) {
         return position[0] >= 1 && position[0] <= width && position[1] >= 1 &&
             position[1] <= width;
@@ -57,9 +56,23 @@ export default function Game() {
 
     useEffect(() => {
         generateBoard();
-        window.addEventListener("keydown", (e) => {
-            // @ts-ignore
+    }, []);
+
+    useEffect(() => {
+        // Update the ref whenever board state changes
+        boardRef.current = board;
+    }, [board]);
+
+    useEffect(() => {
+        function handleKeyDown(e: KeyboardEvent) {
+            const isEmptyByPosition = function (position: number[]) {
+                let index = convertPositionToIndex(position);
+                // console.log({index})
+                return board![index] === -1;
+            };
+
             let position = convertIndexToPosition(selectedTileIndex);
+            console.log(selectedTileIndex)
             let newPosition;
             if (e.key === "ArrowUp") {
                 newPosition = [position[0] - 1, position[1]];
@@ -68,24 +81,28 @@ export default function Game() {
             } else if (e.key === "ArrowLeft") {
                 newPosition = [position[0], position[1] - 1];
             } else if (e.key === "ArrowRight") {
-                newPosition = [position[0] + 1, position[1]];
+                newPosition = [position[0], position[1] + 1];
             }
-            if (!isValidPosition) {
+
+            if (!isValidPosition(newPosition)) {
                 return;
             }
+
             if (isEmptyByPosition(newPosition)) {
                 moveTileToEmpty(selectedTileIndex);
             }
-            let newIndex = convertPositionToIndex(newPosition);
-            setselectedTileIndex(newIndex);
 
-            if (
-                isValidPosition(newPosition) && isEmptyByPosition(newPosition)
-            ) {
-                moveTileToEmpty(selectedTileIndex);
-            }
-        });
-    }, []);
+            let newIndex = convertPositionToIndex(newPosition);
+            console.log(newIndex)
+            setselectedTileIndex(newIndex);
+        }
+
+        window.addEventListener("keydown", handleKeyDown);
+
+        return () => {
+            window.removeEventListener("keydown", handleKeyDown);
+        };
+    }, [board,selectedTileIndex]);
 
     const handleTileClick = (tileNumber: number) => {
         let tileIndex = (board?.indexOf(tileNumber) as number + 1) as number;
@@ -123,28 +140,35 @@ export default function Game() {
 
     return (
         <div className="puzzle-container">
-            <h2 className="text-center text-xl font-bold">
-                Use the arrow keys to move the tiles. Press Enter to make a
-                move.
-            </h2>
-            <div className="flex items-center justify-center space-x-4">
-                <label className="relative inline-block w-14 h-8">
-                    <input
-                        type="checkbox"
-                        className="absolute opacity-0 w-0 h-0"
-                        onChange={handleSwitch}
-                    />
-                    <span className="block w-14 h-8 bg-gray-300 rounded-full cursor-pointer transition-colors duration-300">
-                        <span
-                            className={`block w-6 h-6 bg-white rounded-full shadow-md transform transition-transform duration-300 ${
-                                width === 4 ? "translate-x-6" : "translate-x-0"
-                            }`}
-                        >
+            <div className="flex justify-between">
+                <h2 className="text-center text-xl font-bold">
+                    Use the arrow keys to move the tiles. Press Enter to make a
+                    move.
+                </h2>
+                <div className="flex items-center justify-center space-x-4">
+                    <label className="relative inline-block w-14 h-8">
+                        <input
+                            type="checkbox"
+                            className="absolute opacity-0 w-0 h-0"
+                            onChange={handleSwitch}
+                        />
+                        <span className="block w-14 h-8 bg-gray-300 rounded-full cursor-pointer transition-colors duration-300">
+                            <span
+                                className={`block w-6 h-6 bg-white rounded-full shadow-md transform transition-transform duration-300 ${
+                                    width === 4
+                                        ? "translate-x-6"
+                                        : "translate-x-0"
+                                }`}
+                            >
+                            </span>
                         </span>
-                    </span>
-                </label>
-                <p className="text-xl font-semibold">{width * width} Tiles</p>
+                    </label>
+                    <p className="text-xl font-semibold">
+                        {width * width} Tiles
+                    </p>
+                </div>
             </div>
+
             <div
                 className={[
                     "tiles-wrapper  ",
--- app/slide-puzzle/page.tsx
+++ app/slide-puzzle/page.tsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React from "react";
 import Game from "./game";
+import Link from "next/link";
 export default function Page() {
   return (
     <div>
       <div className="container mx-auto px-4 py-6">
-      <h1 className="text-3xl font-bold mb-4 text-center"> Number Slide Puzzle </h1>
-      <Game />
+        <div className="flex">
+          <Link
+            href="/games"
+            className="inline-block text-lg text-blue-600 hover:text-blue-800 mb-6"
+          >
+            &larr; Back to Games
+          </Link>
+          <h1 className="text-3xl font-bold mb-4 text-center">
+            Number Slide Puzzle
+          </h1>
+        </div>
+        <div>
+          <Game />
+        </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
-
-
-
-
